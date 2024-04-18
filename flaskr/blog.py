79d523dbe80fc648cc4e82b8415c8109dad8d104
yuta0507 <yutakikkawa0507@gmail.com>
2024-04-18 16:52:18 +1000

    Implement the top page template

---
+++ flaskr/blog.py
@@ -0,0 +1,12 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+from flaskr.db import get_db
+
+bp = Blueprint('blog', __name__)
+
+@bp.route('/')
+def index():
+    return render_template('blog/index.html')
