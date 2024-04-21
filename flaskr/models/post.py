edd7a5600eb0b076675a19dd24aabc18ad4db0a3
yuta0507 <yutakikkawa0507@gmail.com>
2024-04-21 21:18:46 +1000

    Add post.py

---
+++ flaskr/models/post.py
@@ -0,0 +1,28 @@
+from flaskr.db import get_db
+
+from flaskr.utils.card_image import CardImage
+
+class Post(object):
+    def __init__(self) -> None:
+        self.db = get_db()
+
+    def store(self, vals: dict) -> None:
+        card_image = CardImage
+
+        self.db.execute(
+            'INSERT INTO posts' +
+            '(store_name, title, description, body, card_image_path, category_id, location_id, publish_flag)' +
+            'VALUES(?, ?, ?, ?, ?, ?, ?, ?)',
+            (
+                vals['store_name'],
+                vals['title'],
+                vals['description'],
+                vals['body'],
+                card_image.generate_path(),
+                vals['category_id'],
+                vals['location_id'],
+                vals['publish_flag']
+            )
+        )
+
+
