bbb43bbbecd8d341ff0f8dfb69f53e2ee4374a61
yuta0507 <yutakikkawa0507@gmail.com>
2024-04-21 22:58:42 +1000

    Fix validation.py

--- flaskr/utils/validation.py
+++ flaskr/utils/validation.py
@@ -1,13 +1,11 @@
 class Validation(object):
     error_message = {}
+    has_error = False
 
     def __init__(self, rule: dict, form: dict) -> None:
         self.rule = rule
         self.form = form
         self.__validate()
-
-    def has_error(self) -> bool:
-        return self.error_message != {}
 
     def __validate(self) -> None:
         for name, value in self.form.items():
@@ -15,8 +13,9 @@ class Validation(object):
                 if rule == 'required':
                     self.error_message[name] = self.__required(value)
 
-    def __required(self, value) -> str:
+    def __required(self, value) -> str | None:
         if value == '':
+            self.has_error = True
             return '*This field is required'
         else:
-            return ''
+            return None
