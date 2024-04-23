c091ffe45b79a5efef29109eddf2113ca7276510
yuta0507 <yutakikkawa0507@gmail.com>
2024-04-23 21:13:19 +1000

    Fix vaidation logic

--- flaskr/utils/validation.py
+++ flaskr/utils/validation.py
@@ -2,20 +2,39 @@ class Validation(object):
     error_message = {}
     has_error = False
 
-    def __init__(self, rule: dict, form: dict) -> None:
+    def __init__(self, rule: dict, request) -> None:
         self.rule = rule
-        self.form = form
+        self.request = request
         self.__validate()
 
     def __validate(self) -> None:
-        for name, value in self.form.items():
-            for rule in self.rule[name]:
+        for name, rules in self.rule.items():
+            for rule in rules:
                 if rule == 'required':
-                    self.error_message[name] = self.__required(value)
+                    self.error_message[name] = self.__required(name)
 
-    def __required(self, value) -> str:
-        if value == '':
+                if rule == 'required:file':
+                    self.error_message[name] = self.__required_file(name)
+
+    def __required(self, name) -> str:
+        error_message = '*This field is required'
+
+        if name not in self.request.form:
             self.has_error = True
-            return '*This field is required'
+            return error_message
+        elif self.request.form[name] == '':
+            self.has_error = True
+            return error_message
+        else:
+            return ''
+
+    def __required_file(self, name) -> str:
+        error_message = '*This field is required'
+
+        is_file_uploaded = (name in self.request.files and self.request.files[name].filename != '')
+
+        if not is_file_uploaded:
+            self.has_error = True
+            return error_message
         else:
             return ''
